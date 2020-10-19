58e3cf3d2189f969ef57afc61a40d72d1189b069
jiwon seo <64701875+jwseo4074@users.noreply.github.com>
2020-10-19 19:22:18 +0900

    Update and rename robocop(b).cpp to robocop(c).cpp

--- robocop(c).cpp
+++ robocop(c).cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int f_x=0;
+int f_x = 0;
 int f_y=0;
 int cnt=0;
 vector <int> v1;
 vector <int> v2;
 
-void set_func(){
+void set_func() {
     v1.push_back(f_x);
     v2.push_back(f_y);
     cnt++;
 }
-int main()
-{
+int main()  {
     int n_K;
     cin >> n_K;
     int n_xy[n_K+1][2];
@@ -23,7 +21,6 @@ int main()
         cin >> n_xy[j][0];
         cin >> n_xy[j][1];
     }
-
     f_x = n_xy[0][0];
     f_y = n_xy[0][1];
     n_xy[n_K][0] = n_xy[0][0];
@@ -36,18 +33,16 @@ int main()
     for(int k=0;k<5;k++)
         cin >> arr_t[k];
     int v_xy;
-
     for(int i=0;i<n_K;i++){
-        if(n_xy[i][0] == n_xy[i+1][0]){       //x 좌표가 같을때 -> y 비교
-            if (n_xy[i][1] < n_xy[i+1][1] ){      // y 가 커질때
-                //v_xy = abs(n_xy[i+1][1]-n_xy[i][1]);
+        if(n_xy[i][0] == n_xy[i+1][0]){
+            if (n_xy[i][1] < n_xy[i+1][1] ){
                 v_xy = abs(n_xy[i][1]-n_xy[i+1][1]);
                 for(int j=0;j<v_xy;j++){
                     f_y++;
                     set_func();
                 }
             }
-            if (n_xy[i][1] > n_xy[i+1][1]){       // y 가 작아질때
+            if (n_xy[i][1] > n_xy[i+1][1]){
                 v_xy = abs(n_xy[i][1]-n_xy[i+1][1]);
                 for(int j=0;j<v_xy;j++){
                     f_y--;
@@ -56,16 +51,15 @@ int main()
 
             }
         }
-        if(n_xy[i][1] == n_xy[i+1][1]){     //y 좌표가 같을때 -> x 비교
-            if (n_xy[i][0] < n_xy[i+1][0] ){      // x 가 커질때
-                //v_xy = abs(n_xy[i+1][0]-n_xy[i][0]);
+        if(n_xy[i][1] == n_xy[i+1][1]){
+            if (n_xy[i][0] < n_xy[i+1][0] ){
                 v_xy = abs(n_xy[i][0]-n_xy[i+1][0]);
                 for(int j=0;j<v_xy;j++){
                     f_x++;
                     set_func();
                 }
             }
-            if (n_xy[i][0] > n_xy[i+1][0] ){      // y 가 작아질때
+            if (n_xy[i][0] > n_xy[i+1][0] ){
                 v_xy = abs(n_xy[i][0]-n_xy[i+1][0]);
                 for(int j=0;j<v_xy;j++){
                     f_x--;
@@ -74,7 +68,6 @@ int main()
             }
         }
     }
-
     for (int j=0; j<5;j++)
         arr_t[j] = arr_t[j] % cnt ;
     for (int j=0; j<5;j++)
